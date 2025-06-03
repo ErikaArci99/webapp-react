d61852e405778456633b2ae83c7801684f4a69a3
ErikaArci99 <erikaarci1999@outlook.it>
2025-06-04 00:59:11 +0200

    [UPD] configurate le rotte principali con layout esterno

--- webapp-react/src/App.jsx
+++ webapp-react/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import MovieDetail from "./pages/MovieDetail"
+import Layout from "./components/Layout"
 
 function App() {
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+      {/* layout visibile ovunque */}
+      <Layout />
+
+      {/* rotte principali */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+
