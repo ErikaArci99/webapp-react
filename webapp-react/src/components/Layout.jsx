835d9b9b343dc7b50cd7e5ddfd36774c982215a4
ErikaArci99 <erikaarci1999@outlook.it>
2025-06-04 00:58:22 +0200

    [UPD] creato layout base visibile sempre

---
+++ webapp-react/src/components/Layout.jsx
@@ -0,0 +1,10 @@
+function Layout() {
+    return (
+        <div className="container mt-4">
+            {/* layout base visibile in tutte le pagine */}
+            <p>layout base - qui ci sarà la navbar</p>
+        </div>
+    )
+}
+
+export default Layout
