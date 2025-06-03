c8a1229adaf1418c849f6d1d81c80f92e951846d
ErikaArci99 <erikaarci1999@outlook.it>
2025-06-04 00:57:29 +0200

    [UPD] create le pagine Home e MovieDetail con contenuto statico

---
+++ webapp-react/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="container mt-4">
+            {/* pagina home con lista film */}
+            <h1>Home - Lista Film</h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ webapp-react/src/pages/MovieDetail.jsx
@@ -0,0 +1,10 @@
+function MovieDetail() {
+    return (
+        <div className="container mt-4">
+            {/* pagina dettaglio film */}
+            <h1>Dettaglio Film</h1>
+        </div>
+    )
+}
+
+export default MovieDetail
