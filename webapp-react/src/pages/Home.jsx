b78c82aef63ea9bdf8b9302bb612179d6cc88f84
ErikaArci99 <erikaarci1999@outlook.it>
2025-06-04 02:10:22 +0200

    [UPD] aggiunta chiamata axios per recuperare film dal backend e render nella home

--- webapp-react/src/pages/Home.jsx
+++ webapp-react/src/pages/Home.jsx
@@ -1,23 +1,39 @@
-import movies from "../data/movies"
-import MovieCard from "../components/MovieCard"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+    // stato per la lista dei film, inizialmente vuoto
+    const [movies, setMovies] = useState([]);
+
+    // carica i film dal backend appena il componente monta
+    useEffect(() => {
+        axios.get("http://localhost:3000/movies")
+            .then((resp) => {
+                setMovies(resp.data); // salva i film nello stato
+            })
+            .catch((err) => {
+                console.log("Errore nel caricamento dei film", err);
+            });
+    }, []);
+
     return (
         <div className="container mt-4">
-            {/* pagina home con lista film */}
+            {/* titolo pagina */}
             <h1>Lista Film</h1>
 
             <div className="row">
-                {/* ciclo su ogni film e uso la componente */}
-                {movies.map((movie) => (
-                    <div className="col-md-4 mb-4" key={movie.id}>
-                        <MovieCard movie={movie} />
+                {/* ciclo sui film e uso MovieCard */}
+                {movies.map((film) => (
+                    <div className="col-md-4 mb-4" key={film.id}>
+                        <MovieCard movie={film} />
                     </div>
                 ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
+
 
