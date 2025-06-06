5bf98a11c9af636675608bb4bcbcf88da63fdef7
ErikaArci99 <erikaarci1999@outlook.it>
2025-06-06 16:32:10 +0200

    [UPD] wrappato il loader in main

--- webapp-react/src/main.jsx
+++ webapp-react/src/main.jsx
@@ -4,9 +4,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { LoaderProvider } from './contexts/LoaderContext' // importa il provider
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LoaderProvider>
+      <App />
+    </LoaderProvider>
   </StrictMode>,
 )
+
