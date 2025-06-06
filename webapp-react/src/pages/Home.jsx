e595408046810b147f8293768061d0210ae76bbb
ErikaArci99 <erikaarci1999@outlook.it>
2025-06-06 16:35:08 +0200

    [DONE] esercizio terminato

--- webapp-react/src/pages/Home.jsx
+++ webapp-react/src/pages/Home.jsx
@@ -1,19 +1,28 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import MovieCard from "../components/MovieCard";
+import { useLoader } from "../contexts/LoaderContext"; // importa il context
 
 function Home() {
     // stato per la lista dei film, inizialmente vuoto
     const [movies, setMovies] = useState([]);
 
+    // ottengo la funzione per attivare/disattivare il loader
+    const { setLoading } = useLoader();
+
     // carica i film dal backend appena il componente monta
     useEffect(() => {
+        setLoading(true); // mostra il loader
+
         axios.get("http://localhost:3000/movies")
             .then((resp) => {
                 setMovies(resp.data); // salva i film nello stato
             })
             .catch((err) => {
                 console.log("Errore nel caricamento dei film", err);
+            })
+            .finally(() => {
+                setLoading(false); // nascondi il loader
             });
     }, []);
 
@@ -35,5 +44,3 @@ function Home() {
 }
 
 export default Home;
-
-
